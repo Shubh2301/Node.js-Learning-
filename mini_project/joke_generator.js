e09c00562c4327a137d1c1bcbe81e43970ec131e
Shubham Kamble <shubhamkamble3011@gmail.com>
2026-06-17 13:07:40 +0530

    joke-generator

---
+++ mini_project/joke_generator.js
@@ -0,0 +1,24 @@
+import https from "https";
+import chalk  from "chalk";
+
+
+
+const getJoke=()=>{
+    const url='https://official-joke-api.appspot.com/random_joke';
+
+    https.get(url,(response)=>{
+        let data=""
+        response.on('data',(chunk)=>{
+           data+=chunk
+        })
+        response.on('end',()=>{
+            const joke=JSON.parse(data);
+            console.log("Here is a random joke")
+            console.log(chalk.red.bold(`${joke.setup}`))
+            console.log(chalk.blue.bgGreen.bold(`${joke.punchline}`))
+              
+        })
+    })
+    
+}
+getJoke()
